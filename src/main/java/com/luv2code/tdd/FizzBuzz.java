5db4f3d9c758b5d6d4e70cad52ede86de0b3c0f5
arturich <arturich@gmail.com>
2022-04-26 15:52:55 -0500

    Refactoring code with confidence of test cases

--- src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -3,19 +3,40 @@ package com.luv2code.tdd;
 public class FizzBuzz {
 
 	// If number is divisible by 3, print Fizz
-	public static String compute(int num) {
+	
+	
+public static String compute(int num) {
+	
+	StringBuffer strBuff = new StringBuffer();
 		
 		if(num % 3 == 0 && num % 5 == 0)
-			return "FizzBuzz";
+			strBuff.append("FizzBuzz");
 		else
 		if(num % 3 == 0)
-			return "Fizz";
+			strBuff.append("Fizz");
 		else
 		if(num % 5 == 0)
-			return "Buzz";
+			strBuff.append("Buzz");
+		else
+			strBuff.append(num);
 		
-		return Integer.toString(num);
+		return strBuff.toString();
 	}
+	
+	
+//	public static String compute(int num) {
+//		
+//		if(num % 3 == 0 && num % 5 == 0)
+//			return "FizzBuzz";
+//		else
+//		if(num % 3 == 0)
+//			return "Fizz";
+//		else
+//		if(num % 5 == 0)
+//			return "Buzz";
+//		
+//		return Integer.toString(num);
+//	}
 
 	
 }
